73abe0aae9b5f5ca20f146d821c3643ed49aa6bc
snowjang24 <asdka4618@gmail.com>
2019-06-14 17:48:26 +0900

    3. amazon carousel clone - right button클릭 시 무한 carousel 대략적인 로직 구현
    right button 클릭 시 carousel 무한으로 작동하게 구현, 대략적인 로직까지 구현
    리팩토링을 통해 코드 깔끔하게 수정할 예정

--- Carousel.js
+++ Carousel.js
@@ -1,20 +1,57 @@
 class Carousel {
-  constructor({ leftBtn, rightBtn }) {
+  constructor({ leftBtn, rightBtn, cardNumber }) {
     this.leftBtn = leftBtn;
     this.rightBtn = rightBtn;
+    this.cardNumber = cardNumber;
+    this.currentIndex = 0;
   }
+
   init() {
-    this.registerEvensts();
+    this.registerEvents();
   }
-  onClickHandler() {
-    document.querySelector("");
+
+  onClickHandler(event) {
+    event.preventDefault();
+    this.currentIndex = (this.currentIndex + 1) % this.cardNumber;
+    console.log(this.currentIndex);
+    // for (let i; i < this.cardNumber; i++) {
+    //   document.querySelector(
+    //     `.item:nth-child(${((this.currentIndex + ((i + 3) % 4)) % 4) + 1})`
+    //   ).style.transform = `translateX(${toString(-150 + i * 100)}%)`;
+    // }
+    document.querySelector(
+      `.item:nth-child(${((this.currentIndex + 3) % 4) + 1})`
+    ).style.transform = `translateX(-150%)`;
+    document.querySelector(
+      `.item:nth-child(${(this.currentIndex % 4) + 1})`
+    ).style.transform = `translateX(-50%)`;
+    document.querySelector(
+      `.item:nth-child(${((this.currentIndex + 1) % 4) + 1})`
+    ).style.transform = `translateX(50%)`;
+    document.querySelector(
+      `.item:nth-child(${((this.currentIndex + 2) % 4) + 1})`
+    ).style.transform = `translateX(150%)`;
+
+    document.querySelector(
+      `.item:nth-child(${(this.currentIndex % 4) + 1})`
+    ).style.opacity = `1`;
+    document.querySelector(
+      `.item:nth-child(${((this.currentIndex + 1) % 4) + 1})`
+    ).style.opacity = `0`;
+    document.querySelector(
+      `.item:nth-child(${((this.currentIndex + 2) % 4) + 1})`
+    ).style.opacity = `0`;
+    document.querySelector(
+      `.item:nth-child(${((this.currentIndex + 3) % 4) + 1})`
+    ).style.opacity = `0`;
   }
   registerEvents() {
-    this.leftBtn.addEventListner("onclick", this.onClickHandler());
+    this.rightBtn.addEventListener("click", this.onClickHandler.bind(this));
   }
 }
 
-const leftBtn = documnet.querySelector(".contents__button");
-const carousel = new Carousel({ leftBtn, rightBtn });
-
+const leftBtn = document.querySelector(".contents__button_left");
+const rightBtn = document.querySelector(".contents__button_right");
+const cardNumber = 4;
+const carousel = new Carousel({ leftBtn, rightBtn, cardNumber });
 carousel.init();
