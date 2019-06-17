ea78a0483191fa47ddcf86d7f8ec798b204e0712
snowjang24 <asdka4618@gmail.com>
2019-06-17 17:02:05 +0900

    5. amazon carousel clone - 카로셀 기능 구현 완료 onClickHandler를 index를 움직이는 부분과 cardPosition을 그리는 부분으로 나누어 구현 style을 item의 nth child에 맞게 뿌리도록 구현, transform과 opacity를 통해 carousel 작동을 구현

--- Carousel.js
+++ Carousel.js
@@ -4,15 +4,18 @@ class Carousel {
     this.rightBtn = rightBtn;
     this.cardNumber = cardNumber;
     this.currentIndex = 0;
+    this.selectedDirection = undefined;
   }
 
-  addStyleToItem(index) {
+  addStyleToItem(i) {
+    const convertedIndex =
+      this.currentIndex + ((i + (this.cardNumber - 1)) % this.cardNumber);
+    const nthChildNumb = (convertedIndex % this.cardNumber) + 1;
+
     let currentItemStyle = document.querySelector(
-      `.item:nth-child(${((this.currentIndex +
-        ((index + (this.cardNumber - 1)) % this.cardNumber)) %
-        this.cardNumber) +
-        1})`
+      `.item:nth-child(${nthChildNumb})`
     ).style;
+
     currentItemStyle.transform = `translateX(${-150 + i * 100}%)`;
     currentItemStyle.opacity = "0";
     if (i === 1) {
@@ -25,29 +28,34 @@ class Carousel {
       direction === "left" ? this.currentIndex - 1 : this.currentIndex + 1;
     if (index < 0) index += this.cardNumber;
     this.currentIndex = index % this.cardNumber;
-    console.log(this.currentIndex);
   }
 
-  onClickHandler(event) {
-    const direction = event.target.id;
-    event.preventDefault();
-    this.moveCurrentIndex(direction);
+  drawCardPosition() {
     for (let i = 0; i < this.cardNumber; i++) {
       this.addStyleToItem(i);
     }
+  }
+
+  onClickHandler(event) {
+    this.selectedDirection = event.target.id;
+    event.preventDefault();
+    this.moveCurrentIndex(this.selectedDirection);
+    this.drawCardPosition();
   }
 
   registerEvents() {
     this.leftBtn.addEventListener("click", this.onClickHandler.bind(this));
     this.rightBtn.addEventListener("click", this.onClickHandler.bind(this));
   }
+
   init() {
     this.registerEvents();
+    this.drawCardPosition();
   }
 }
 
 const leftBtn = document.querySelector(".contents__button_left");
 const rightBtn = document.querySelector(".contents__button_right");
-const cardNumber = 4;
+const cardNumber = document.querySelectorAll(".contents__item").length;
 const carousel = new Carousel({ leftBtn, rightBtn, cardNumber });
 carousel.init();
