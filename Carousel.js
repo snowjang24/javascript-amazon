ad0fd7c69ac778c8efc7a282d33943053018ac8f
snowjang24 <asdka4618@gmail.com>
2019-06-20 15:24:38 +0900

    캐러샐 리팩토링으로 코드 간소화 및 디자인 추가
    캐러셀의 코드를 조금 더 보기 쉽게 변수 명 짓고 수정
    디자인 추가

--- Carousel.js
+++ Carousel.js
@@ -1,39 +1,43 @@
 class Carousel {
-  constructor({ leftBtn, rightBtn, cardNumber }) {
+  constructor({ leftBtn, rightBtn, numberOfCard }) {
     this.leftBtn = leftBtn;
     this.rightBtn = rightBtn;
-    this.cardNumber = cardNumber;
+    this.numberOfCard = numberOfCard;
     this.currentIndex = 0;
     this.selectedDirection = undefined;
   }
 
+  makeRotateNumb(number) {
+    return number % this.numberOfCard;
+  }
+
   addStyleToItem(i) {
     const convertedIndex =
-      this.currentIndex + ((i + (this.cardNumber - 1)) % this.cardNumber);
-    const nthChildNumb = (convertedIndex % this.cardNumber) + 1;
-
+      this.currentIndex + this.makeRotateNumb(i + (this.numberOfCard - 1));
+    const nthChildNumb = this.makeRotateNumb(convertedIndex) + 1;
     let currentItemStyle = document.querySelector(
       `.item:nth-child(${nthChildNumb})`
     ).style;
 
     currentItemStyle.transform = `translateX(${-150 + i * 100}%)`;
-    currentItemStyle.opacity = "0";
-    if (i === 1) {
-      currentItemStyle.opacity = "1";
-    }
+    currentItemStyle.transition = "all .2s linear";
+    currentItemStyle.opacity = i === 1 ? "1" : "0";
   }
 
   moveCurrentIndex(direction) {
     let index =
       direction === "left" ? this.currentIndex - 1 : this.currentIndex + 1;
-    if (index < 0) index += this.cardNumber;
-    this.currentIndex = index % this.cardNumber;
+    if (index < 0) index += this.numberOfCard;
+    this.currentIndex = this.makeRotateNumb(index);
   }
 
   drawCardPosition() {
-    for (let i = 0; i < this.cardNumber; i++) {
+    for (let i = 0; i < this.numberOfCard; i++) {
       this.addStyleToItem(i);
     }
+  }
+  makeRandomIndex() {
+    return Math.floor(Math.random() * (this.numberOfCard - 1));
   }
 
   onClickHandler(event) {
@@ -49,13 +53,14 @@ class Carousel {
   }
 
   init() {
-    this.registerEvents();
+    this.currentIndex = this.makeRandomIndex();
     this.drawCardPosition();
+    this.registerEvents();
   }
 }
 
 const leftBtn = document.querySelector(".contents__button_left");
 const rightBtn = document.querySelector(".contents__button_right");
-const cardNumber = document.querySelectorAll(".contents__item").length;
-const carousel = new Carousel({ leftBtn, rightBtn, cardNumber });
+const numberOfCard = document.querySelectorAll(".contents__item").length;
+const carousel = new Carousel({ leftBtn, rightBtn, numberOfCard });
 carousel.init();
